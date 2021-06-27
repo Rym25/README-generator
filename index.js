a6694eb3b80e844170fabca3fed0d9820d5517f8
Ryely <ryely.moore@outlook.com>
2021-06-27 13:17:56 -0600

    commiting changes to create a new branch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -152,7 +153,9 @@ const questions = [{
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
